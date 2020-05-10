64bea5d0bf980e3f12b9b2ce449eb164a5bb0800
Jose Carlos Paniagua <jcpmcode@gmail.com>
2020-05-10 02:01:06 -0500

    Se agrgó la lectura de datos.

--- HolaMundo/src/main/java/HolaMundo.java
+++ HolaMundo/src/main/java/HolaMundo.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class HolaMundo {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String usuario;
+        // Declaracion de variables
+        String saludo = "Hola";
+        String nombre = "Jose";
+
         System.out.println("Hola Mundo");
+
+        // Concatenación
+        System.out.println(saludo + " " + nombre);
+
+        // Leer datos
+        System.out.println("Ingresa tu nombre completo: ");
+        usuario = scanner.nextLine();
+
+        System.out.println("El usuario que ingresaste fue: " + usuario);
     }
 }
